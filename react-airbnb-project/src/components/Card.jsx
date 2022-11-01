afa305d8faa0ac46dc0e64136b374c59f517f859
JoasR <joas.rothig@hotmail.com>
2022-11-01 17:00:25 +0100

    implemented props

--- react-airbnb-project/src/components/Card.jsx
+++ react-airbnb-project/src/components/Card.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import cardPhoto from "../assets/katie-zaferes.png"
 import starIcon from "../assets/star.png"
 
-function Card(){
+function Card(props){
     return(
         <div className="card-container">
             <p className="card-sold">SOLD OUT</p>
-            <img className="card-photo" src={ cardPhoto } alt="" />
+            <img className="card-photo" src={`../../src/assets/${props.img}`} alt="" />
             <div className="card-rating">
                 <img className="card-star-icon" src={ starIcon } alt="" />
-                <h4><span className="bold">5.0</span> (6)-USA</h4>
+                <h4><span className="bold">{props.rating}</span> ({props.reviewCount}) - {props.country}</h4>
             </div>
-            <p className="card-text">Life lessons with katie Zaferes</p>
-            <h3 className="card-price"><span className="bold">From €136</span> / person</h3>
+            <p className="card-text">{props.title}</p>
+            <h3 className="card-price"><span className="bold">From €{props.price}</span> / person</h3>
         </div>
     )
 }
