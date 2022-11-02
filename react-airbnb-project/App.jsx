67a63d7f1002adf80a29e8b056d46b6eaa82b8d9
JoasR <joas.rothig@hotmail.com>
2022-11-02 16:25:07 +0100

    Loaded in data from external file

--- react-airbnb-project/App.jsx
+++ react-airbnb-project/App.jsx
@@ -2,20 +2,36 @@ import React from "react"
 import Card from "./src/components/Card"
 import Hero from "./src/components/Hero"
 import Navbar from "./src/components/Navbar"
+import cardData from "./src/data"
 
 function App(){
+    const cardElement = cardData.map(data => { 
+        return <Card 
+            key={data.id}
+            // img={data.coverImg}
+            // rating={data.stats.rating}
+            // reviewCount={data.stats.reviewCount}
+            // location={data.location}
+            // title={data.title}
+            // price={data.price}
+            // openSpots={data.openSpots}
+            //
+            //OR
+            //
+            //{...data}
+            //OR
+            data = {data}
+        />
+    })
+    
+   
     return(
         <>
             <Navbar />
             <Hero />
-            <Card 
-                img="katie-zaferes.png"
-                rating="5.0"
-                reviewCount={6}
-                country="USA"
-                title="Life lessons with Katie Zaferes"
-                price={136}
-            />
+            <div className="cards-list">
+                {cardElement}
+            </div>
         </>
     )
   }
--- react-airbnb-project/src/components/Card.jsx
+++ react-airbnb-project/src/components/Card.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import starIcon from "../assets/star.png"
 
 function Card(props){
+    let badgeText
+    if (props.data.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if(props.data.location === "Online"){
+        badgeText = "ONLINE"
+    }
     return(
         <div className="card-container">
-            <p className="card-sold">SOLD OUT</p>
-            <img className="card-photo" src={`../../src/assets/${props.img}`} alt="" />
+            {badgeText && <p className="card-sold">{badgeText}</p>}
+            <img className="card-photo" src={`../../src/assets/${props.data.coverImg}`} alt="" />
             <div className="card-rating">
                 <img className="card-star-icon" src={ starIcon } alt="" />
-                <h4><span className="bold">{props.rating}</span> ({props.reviewCount}) - {props.country}</h4>
+                <h4><span className="bold">{props.data.stats.rating.toFixed(1)}</span> ({props.data.stats.reviewCount}) - {props.data.location}</h4>
             </div>
-            <p className="card-text">{props.title}</p>
-            <h3 className="card-price"><span className="bold">From €{props.price}</span> / person</h3>
+            <p className="card-text">{props.data.title}</p>
+            <h3 className="card-price"><span className="bold">From €{props.data.price}</span> / person</h3>
         </div>
     )
 }
