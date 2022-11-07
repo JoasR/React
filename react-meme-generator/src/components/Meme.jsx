d4fcbea31947ee3ef91b66a036b47cd8223796c7
JoasR <joas.rothig@hotmail.com>
2022-11-07 17:56:55 +0100

    updated the retrieval of images

--- react-meme-generator/src/components/Meme.jsx
+++ react-meme-generator/src/components/Meme.jsx
@@ -2,23 +2,33 @@ import React from "react";
 import memesData from "../data/memesData"
 
 function Meme(){
-    const [memeImage, setMemeImage] = React.useState("");
-    function handleClick(){
-        const memesArray = memesData.data.memes;
-        let randomNumberInDataRange = Math.round(Math.random() * memesArray.length);
-        // const memeUrl = memesArray[randomNumberInDataRange].url
-        // console.log(memeUrl);
-        
-        setMemeImage(memesArray[randomNumberInDataRange].url)
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/1ur9b0.jpg"
+    });
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+    
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes;
+        let randomNumber = Math.round(Math.random() * memesArray.length);
+        const url = (memesArray[randomNumber].url)
+        setMeme(prevState => {
+            return {
+                ...prevState,
+                randomImage: url
+            }
+        })
     }
     return(
         <main className="meme-container">
                 <div className="meme-form">
                     <input placeholder="Top Text" className="meme-form-input" type="text" />
                     <input placeholder="Bottom Text" className="meme-form-input" type="text" />
-                    <button className="meme-form-button" onClick={handleClick}>Get a new meme Image 🖼️</button>
+                    <button className="meme-form-button" onClick={getMemeImage}>Get a new meme Image 🖼️</button>
                 </div>
-                <img className="meme-image" src={memeImage} alt="" />
+                <img className="meme-image" src={meme.randomImage} alt="" />
         </main>
     )
 }
