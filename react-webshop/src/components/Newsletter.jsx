ad63c3fe16f4b540edcd867ab61c7db2f4b1e2ea
JoasR <joas.rothig@hotmail.com>
2023-01-11 14:10:25 +0100

    Finished Fontend Design
    Finished Cart page.
    Creating media QUeries for design on phone by creating responsive.js and using media queries in the components

--- react-webshop/src/components/Newsletter.jsx
+++ react-webshop/src/components/Newsletter.jsx
@@ -1,6 +1,7 @@
 import { Send } from '@mui/icons-material'
 import React from 'react'
 import styled from 'styled-components'
+import { mobile } from '../responsive'
 
 const Container = styled.div`
     height: 60vh;
@@ -9,6 +10,7 @@ const Container = styled.div`
     align-items: center;
     justify-content: center;
     flex-direction: column;
+    ${mobile({ padding: "0px 15px" })}
 `
 
 const Title = styled.h1`
@@ -20,6 +22,7 @@ const Description = styled.div`
     font-size: 24px;
     font-weight: 300;
     margin-bottom: 20px;
+    ${mobile({ textAlign: "center" })}
 `
 
 const InputContainer = styled.div`
@@ -30,6 +33,7 @@ const InputContainer = styled.div`
     justify-content: space-between;
     border: 1px solid gray;
     border-right: 0;
+    ${mobile({ width: "80%" })}
 `
 
 const Input = styled.input`
